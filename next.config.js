4f0f39236bd56d03e65d0c10b4520c78797eb1d1
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-01-13 22:21:34 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const withNextIntl = require("next-intl/plugin")("./i18n.ts");
 
 const nextConfig = withNextIntl({});
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push({ bufferutil: "bufferutil", "utf-8-validate": "utf-8-validate", "supports-color": "supports-color" }); 
+    }
+
+    return config;
+  },
+};
